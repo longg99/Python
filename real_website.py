25201a636df6fd8026ba76d89636e8ef2cf58c22
longg99 <vuhoanglong1512@gmail.com>
2022-07-14 12:53:27 -0400

    commit 2

--- real_website.py
+++ real_website.py
@@ -1,9 +1,17 @@
-from cgitb import text
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get("https://www.autotrader.ca/" +
-                         "cars/on/toronto/?rcp=0&rcs=0&prx=100" +
-                         "&prv=Ontario&loc=Toronto%2C%20ON&hprc=True&wcp=True&iosp=True&sts=New-Used&inMarket=basicSearch&make=Honda").text
+html_text = requests.get("https://www.kijijiautos.ca/cars/sedan/").text
 
-print(html_text)
+soup = BeautifulSoup(html_text, 'html.parser')
+cars = soup.find_all("div", class_='b1yLWE bcN7dZ')
+for car in cars:
+    car_name = car.find("h2", class_="G2jAym fcN7dZ z2jAym p2jAym b2jAym").text
+    price = car.find(
+        "span", class_="G2jAym d3uM7V C2jAym p2jAym b2jAym").text.replace("*", "")
+    # link = car.find('a', class_="bcNN7t").text
+
+    print(f'''
+    Car name: {car_name}
+    Price: {price} 
+    ''')
